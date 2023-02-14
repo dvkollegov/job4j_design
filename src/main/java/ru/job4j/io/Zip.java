df718dfcd926c9edc8ed8a1dbb80723d412adb80
Dima <dvkollegov@mail.ru>
2023-02-14 11:02:39 +0300

    14. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,64 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile().getPath()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkZipArgs(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Root folder has a value not equal to three. Usage ROOT_FOLDER.");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        if (!Files.isDirectory(Paths.get(argsName.get("d")))) {
+            throw new IllegalArgumentException("Root folder address is not set correctly. Usage ROOT_FOLDER.");
+        }
+        if (!argsName.get("e").startsWith(".") || argsName.get("e").length() < 2) {
+            throw new IllegalArgumentException("Root folder the wrong file extension was passed. Usage ROOT_FOLDER.");
+        }
+        if (!argsName.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Root folder the wrong file extension was passed.zip. Usage ROOT_FOLDER.");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        checkZipArgs(args);
+        ArgsName arg = ArgsName.of(args);
+        Zip zip = new Zip();
+        List<Path> list = Search.search(Paths.get(arg.get("d")),
+                prd -> !prd.toFile().getName().endsWith(arg.get("e")));
+        zip.packFiles(list, new File(arg.get("o")));
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+    }
+}
