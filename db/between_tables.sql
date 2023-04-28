c5adfefa946d85b17db76ca6d580464eebf34492
Dima <dvkollegov@mail.ru>
2023-04-28 11:08:38 +0300

    0.1. Связи между таблицами [#373615]

---
+++ db/between_tables.sql
@@ -0,0 +1,31 @@
+create table people(
+    id serial primary key,
+    name_people varchar(255),
+    age int,
+    sex varchar(255),
+    car_id int
+);
+
+create table car(
+    id serial primary key,
+    name_car varchar(255),
+    number varchar(255),
+    number_car_id int
+);
+
+create table number_car(
+    id serial primary key,
+    number_car varchar(255)
+);
+
+create table driver(
+    id serial primary key,
+    name_driver varchar(255),
+    experience int
+);
+
+create table bus(
+    id serial primary key,
+    name_bus varchar(255),
+    number_bus varchar(255)
+);
