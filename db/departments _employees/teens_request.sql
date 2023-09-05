be06ec5cd3d626d76a5026ea4cdf5e66879664d7
Dima <dvkollegov@mail.ru>
2023-09-05 14:38:51 +0300

    0. Join [#6862 #442603]

--- db/departments _employees/teens_request.sql
+++ db/departments _employees/teens_request.sql
@@ -1,3 +1,3 @@
-select t1.name as name_1, t2.name as name_2
+select t1.name as name_male, t2.name as name_female
 from teens t1 cross join teens t2
-where t1.gender != t2.gender
+where t1.gender != t2.gender and t1.gender != 'female'
